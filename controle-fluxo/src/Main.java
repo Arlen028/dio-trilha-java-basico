29ad3d0d7a8422dbbc5b0628014f52c9cd2f4110
Arlen Agra <arlens527@gmail.com>
2024-08-23 14:56:07 -0300

    projeto pronto

---
+++ controle-fluxo/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 5, count = 1; do { num += count; System.out.println(num); } while (count <= 3);
+    }
+}
---
+++ java_terminal/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Ola eu fui executado pelo o termial");
+    }
+}
